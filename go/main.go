0e33382b7469dd41db48f341540da56c1dc81d08
Ali Tlekbai <ali.tlekbai@gmail.com>
2021-11-03 10:27:45 +0600

    fix golang

--- go/main.go
+++ go/main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
 	"os"
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	for true {
 		var w, h, playerID, tick int
 		fmt.Scan(&w, &h, &playerID, &tick)
 		// read map
 		for i := 0; i < h; i++ {
-			for j := 0; j < w; j++ {
-				var c rune
-				fmt.Scan(&c)
-			}
+			scanner.Scan()
+			line := scanner.Text()
+			fmt.Fprintln(os.Stderr, line)
 		}
 
 		// number of entities
